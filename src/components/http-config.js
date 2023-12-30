7994601b08a60b79ac9f562e487424ffd64669ee
Duzie <duzzi4x@gmail.com>
2023-12-30 18:58:44 +0100

    amended headers

--- src/components/http-config.js
+++ src/components/http-config.js
@@ -5,7 +5,6 @@ const serverHost = process.env.REACT_APP_SERVER_HOST;
 const getApiHeaders = (accessToken) => {
   return {
     headers: {
-      "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
